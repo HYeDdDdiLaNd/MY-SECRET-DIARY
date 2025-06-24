5c4de504142fbd6c25b2620337c9d0ed51b6c314
HYeDdDiLaNd <anotherHyedddi@gmail.com>
2025-06-24 20:58:16 +0900

    feat: 폰트 추가 및 이미지 추가, 이미지 최적화

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,12 @@ import New from './pages/New';
 import Home from './pages/Home';
 import Notfound from './pages/Notfound';
 
+import emotion1 from './assets/emotion1.png';
+import emotion2 from './assets/emotion2.png';
+import emotion3 from './assets/emotion3.png';
+import emotion4 from './assets/emotion4.png';
+import emotion5 from './assets/emotion5.png';
+
 function App() {
   const nav = useNavigate();
   const onClickEvent = () => {
@@ -12,6 +18,21 @@ function App() {
   };
   return (
     <>
+      <div>
+        <img src={'/emotion1.png'} alt="" />
+        <img src={'/emotion2.png'} alt="" />
+        <img src={'/emotion3.png'} alt="" />
+        <img src={'/emotion4.png'} alt="" />
+        <img src={'/emotion5.png'} alt="" />
+      </div>
+      <div>
+        {/* assets 폴더 내 위치하면 빌드 시 최적화 됨.  브라우저 메모리에 캐싱됨. 단 소수의 이미지만 assets에 개수가 많은 경우 public 폴더 사용*/}
+        <img src={emotion1} alt="" />
+        <img src={emotion2} alt="" />
+        <img src={emotion3} alt="" />
+        <img src={emotion4} alt="" />
+        <img src={emotion5} alt="" />
+      </div>
       <div>
         {/* Routes 밖에 배치된 요소들은 모든 페이지에 공통적으로 보여진다.
         공통적으로 보여지는 요소가 아니면 Routes 안에 배치되어야한다. */}
